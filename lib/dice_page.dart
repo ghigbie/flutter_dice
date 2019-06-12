58a35fa43d689e445bfccd460d0636504719b0c2
ghigbie <gehigbie@gmail.com>
2019-06-11 20:41:01 -0600

    Insert flatt buttons

--- lib/dice_page.dart
+++ lib/dice_page.dart
@@ -1,7 +1,7 @@
 import 'package:dicee/constants.dart';
 import 'package:flutter/material.dart';
 
-class DicePage extends StatelessWidget{
+class DicePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Center(
@@ -12,7 +12,7 @@ class DicePage extends StatelessWidget{
               child: Container(
                 child: Padding(
                   padding: EdgeInsets.all(DICE_PADDING),
-                  child: Image.asset('images/dice1.png')
+                  child: FlatButton(child: Image.asset('images/dice1.png')),
                 ),
               ),
             ),
@@ -21,14 +21,14 @@ class DicePage extends StatelessWidget{
                 child: Container(
                   child: Padding(
                     padding: EdgeInsets.all(DICE_PADDING),
-                    child: Image.asset('images/dice2.png')
+                    child: FlatButton(child: Image.asset('images/dice2.png')),
                   ),
                 ),
               ),
             ),
           ],
-          ),
         ),
+      ),
     );
   }
 }
