60a79fcf3dfbcf32f4937a327f33e8d8ff2d9a3c
ghigbie <gehigbie@gmail.com>
2019-06-11 21:03:11 -0600

    Change var position

--- lib/dice_page.dart
+++ lib/dice_page.dart
@@ -2,11 +2,10 @@ import 'package:dicee/constants.dart';
 import 'package:flutter/material.dart';
 
 class DicePage extends StatelessWidget {
-  var leftDiceNumber = 1;
-  var rightDiceNumber = 2;
-
   @override
   Widget build(BuildContext context) {
+    var leftDiceNumber = 1;
+    var rightDiceNumber = 2;
     return Center(
       child: Container(
         child: Row(
