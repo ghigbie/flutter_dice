0c942caa7cafc68a47bbfa2a26407fa7130d44ce
ghigbie <gehigbie@gmail.com>
2019-06-13 20:42:37 -0600

    Add change in value

--- lib/dice_page.dart
+++ lib/dice_page.dart
@@ -1,7 +1,12 @@
 import 'package:dicee/constants.dart';
 import 'package:flutter/material.dart';
 
-class DicePage extends StatelessWidget {
+class DicePage extends StatefulWidget {
+  @override
+  _DicePageState createState() => _DicePageState();
+}
+
+class _DicePageState extends State<DicePage> {
   int leftDiceNumber = 1;
   int rightDiceNumber = 2;
 
@@ -15,7 +20,8 @@ class DicePage extends StatelessWidget {
               child: Container(
                 child: FlatButton(
                     onPressed: () {
-                      print('Left button was pressed.');
+                      leftDiceNumber = 4;
+                      print('Left button was pressed. Value: $leftDiceNumber' );
                     },
                     child: Image.asset('images/dice$leftDiceNumber.png')),
               ),
@@ -25,7 +31,8 @@ class DicePage extends StatelessWidget {
                 child: Container(
                   child: FlatButton(
                       onPressed: () {
-                        print('Rigth button was pressed.');
+                        rightDiceNumber = 5;
+                        print('Rigth button was pressed. Value: $rightDiceNumber');
                       },
                       child: Image.asset('images/dice$rightDiceNumber.png')),
                 ),
--- lib/main.dart
+++ lib/main.dart
@@ -14,7 +14,7 @@ class DiceeApp extends StatelessWidget {
           title: Text(APP_NAME),
           backgroundColor: Colors.red,
         ),
-        body: DicePage(),
+        body: _DicePageState(),
       )
     );
   }
